28a955fe0c84e21830c348618b47029fd382846c
Orgzales <77345323+Orgzales@users.noreply.github.com>
2024-10-07 20:00:09 -0400

    More sudo code

--- Shower Trauma/Assets/Scripts/DirtyMeterManager.cs
+++ Shower Trauma/Assets/Scripts/DirtyMeterManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class DirtyMeterManager : MonoBehaviour
 {
+    //This script will be incharge of managing the dirty meter of the player
+    //When meter is 0, the player will end the day
+    //When player is in the shower the meter will constantly decrease
+    //When player is washing face, the meter will jump down large amount
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
