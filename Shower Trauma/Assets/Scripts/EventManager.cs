eee176a201945cfd1d5ace57687dc2f164b671ca
Orgzales <77345323+Orgzales@users.noreply.github.com>
2024-10-16 18:42:31 -0400

    Update EventManager.cs

--- Shower Trauma/Assets/Scripts/EventManager.cs
+++ Shower Trauma/Assets/Scripts/EventManager.cs
@@ -6,15 +6,41 @@ public class EventManager : MonoBehaviour
 {
 
 
-    // Start is called before the first frame update
+    public float DirtyMeter = 10.0f;
+    public float DirtyMeterDecreaseRate = 1.0f;
+
+    public float InstanityMeter = 0.0f;
+    public float InstanityMeterIncreaseRate = 0.1f;
+
     void Start()
     {
-
+        StartDecreaseDirtyMeter();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
+
+    //make a function that will decrease the dirty meter by dirtyMeterDecreaseRate every second
+    public void DecreaseDirtyMeter()
+    {
+        DirtyMeter -= DirtyMeterDecreaseRate;
+        Debug.Log("Dirty Meter: " + DirtyMeter);
+        if (DirtyMeter <= 0)
+        {
+            //end the day and stop the decrease of the dirty meter
+            CancelInvoke("DecreaseDirtyMeter");
+            Debug.Log("End of the day");
+        }
+    }
+
+    //call DecreaseDirtyMeter every 2 seconds
+    public void StartDecreaseDirtyMeter()
+    {
+        InvokeRepeating("DecreaseDirtyMeter", 0.0f, 2.0f);
+    }
+
+
+
 }
