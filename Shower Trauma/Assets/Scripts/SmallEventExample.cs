cf8941f9465aed6f313eadd257d8cf29e0f62696
Orgzales <77345323+Orgzales@users.noreply.github.com>
2025-01-06 19:03:04 -0500

    Smallevent start

---
+++ Shower Trauma/Assets/Scripts/SmallEventExample.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallEventExample : MonoBehaviour
+{
+
+    public void PeakingDoor()
+    {
+        Debug.Log("$$PeakingDoor$$");
+    }
+
+    public void Knocking()
+    {
+        Debug.Log("$$Knocking$$");
+    }
+
+    public void Lightsoff()
+    {
+        Debug.Log("$$Lightsoff$$");
+    }
+
+
+}
