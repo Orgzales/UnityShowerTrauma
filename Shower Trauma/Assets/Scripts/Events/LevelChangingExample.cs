0b2c06db3dc29139579c449ee79b98f5493e8d99
Orgzales <77345323+Orgzales@users.noreply.github.com>
2025-01-10 19:18:07 -0500

    Update LevelChangingExample.cs

--- Shower Trauma/Assets/Scripts/Events/LevelChangingExample.cs
+++ Shower Trauma/Assets/Scripts/Events/LevelChangingExample.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class LevelChangingExample : MonoBehaviour
 {
 
+    public void Random_pick()
+    {
+        int RandomEvent = Random.Range(1, 4);
+        if (RandomEvent == 1)
+        {
+            forest();
+        }
+        else if (RandomEvent == 2)
+        {
+            cave();
+
+        }
+        else if (RandomEvent == 3)
+        {
+            secrettunnel();
+        }
+    }
     public void forest()
     {
         Debug.Log("$$Forest$$");
