5d9ddc984185780b7e4ce7d022d186c38c7fba47
Orgzales <77345323+Orgzales@users.noreply.github.com>
2025-01-10 19:17:44 -0500

    Update MediumEventExample.cs

--- Shower Trauma/Assets/Scripts/Events/MediumEventExample.cs
+++ Shower Trauma/Assets/Scripts/Events/MediumEventExample.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class MediumEventExample : MonoBehaviour
 {
 
+
+    public void Random_pick()
+    {
+        int RandomEvent = Random.Range(1, 3);
+        if (RandomEvent == 1)
+        {
+            Lightsturnoff();
+        }
+        else if (RandomEvent == 2)
+        {
+            Windowopens();
+
+        }
+    }
     public void Lightsturnoff()
     {
         Debug.Log("$$LightsTurnOff$$");
