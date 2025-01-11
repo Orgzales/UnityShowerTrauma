5398bc60bd85f7f1d2eb34693901e10a989156e7
Orgzales <77345323+Orgzales@users.noreply.github.com>
2025-01-10 19:19:24 -0500

    Update LargeEventExample.cs

--- Shower Trauma/Assets/Scripts/Events/LargeEventExample.cs
+++ Shower Trauma/Assets/Scripts/Events/LargeEventExample.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class LargeEventExample : MonoBehaviour
 {
+    public void Random_pick()
+    {
+        int RandomEvent = Random.Range(1, 3);
+        if (RandomEvent == 1)
+        {
+            Dontlook();
+        }
+        else if (RandomEvent == 2)
+        {
+            Hide();
+        }
+
+    }
+
     public void Hide()
     {
         Debug.Log("$$Hide$$");
